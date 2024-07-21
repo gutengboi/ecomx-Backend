8bcf17f8e1456c5ad2eb0f4162e05d9e30508e58
gutengboi <animashaunolayinka195@gmail.com>
2024-07-21 01:51:23 +0100

    location updated to required

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    location: { type: String, default: "Lagos, Nigeria" },
+    location: { type: String, required: true },
   },
   { timestamps: true }
 );
